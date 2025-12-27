3e2cc4d89600fd04f34e871ca701291edd7b8cde
ahameed <ahameed@opentext.com>
2025-12-27 15:21:34 +0530

    Added new file practice4.java and practice5.java

---
+++ ProjectStarted/src/gitPractice/practice4.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+public class practice4 {
+    public static void main (String[] args){
+        int b = 1;
+        System.out.println(b);
+
+        int c = 2;
+        System.out.println(c);
+    }
+}
---
+++ ProjectStarted/src/gitPractice/practice5.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+public class practice5 {
+    public static void main (String[] args){
+        int b = 1;
+        System.out.println(b);
+
+        int c = 2;
+        System.out.println(c);
+    }
+}
