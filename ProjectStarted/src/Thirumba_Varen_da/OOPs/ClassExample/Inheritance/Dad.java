2806eb39c40fe8ec76c73881e98df918c5159d1e
ahameed <ahameed@opentext.com>
2025-12-27 11:46:48 +0530

    Deleted newly added unwanted files

--- ProjectStarted/src/Thirumba_Varen_da/OOPs/ClassExample/Inheritance/Dad.java
+++
@@ -1,8 +0,0 @@
-package Thirumba_Varen_da.OOPs.ClassExample.Inheritance;
-
-public class Dad {
-    int dadmoney = 10;
-    void dadproperty(){
-        System.out.println("This is dad property");
-    }
-}
