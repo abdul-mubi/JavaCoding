79f73c681df02143acf7ac846a6886b9299f4f27
ahameed <ahameed@opentext.com>
2025-12-27 11:59:07 +0530

    Added new file practice.java
    Added pactice2 folder

---
+++ ProjectStarted/src/gitPractice/practice.java
@@ -0,0 +1,8 @@
+package gitPractice;
+
+public class practice {
+    public static void main(String[] args){
+        int a=1;
+        System.out.println(a);
+    }
+}
---
+++ ProjectStarted/src/gitPractice/practice2.java
@@ -0,0 +1,8 @@
+package gitPractice;
+
+public class practice2 {
+    public static void main (String[] args){
+        int b = 1;
+        System.out.println(b);
+    }
+}
